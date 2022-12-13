2272436b2190bc62d74fc382e10fda14575cb516
Wolfgang Gassler <wolfgang@gassler.org>
2022-12-13 19:18:33 +0100

    WIP: running extension for chrome

--- src/openpodcast.js
+++ src/openpodcast.js
@@ -1,25 +1,34 @@
 //insert html at the beginning of the website
 function showCredentialsOnWebsite(creds) {
-  let message = "No Spotify credentials found, are you logged in on podcasters.spotify.com? (reload needed)"
+  let message = "No Spotify credentials found, are you logged in on podcasters.spotify.com? (maybe reload this page)"
   if (creds.sp_key && creds.sp_dc) {
-    message = `Spotify Credentials: key: ${creds['sp_key']}, dc: ${creds['sp_dc']}`
+    message = `Spotify keys: key: ${creds['sp_key']}, dc: ${creds['sp_dc']} END`
   }
-  const credsHTMLElement = document.createTextNode(message);
-  document.body.prepend(credsHTMLElement)
+  document.getElementById("openpodcast-plugin").innerHTML = message
 }
 
 //communicate with background script to fetch cookies (only background script is allowed to do this)
 async function retrieveSpotifyCookies() {
+  console.log("retrieving spotify keys")
   // sends a message to the background.js and retrieve cookies
-  const message = await browser.runtime.sendMessage({type: "spotifycookies"})
+  const message = await browser.runtime.sendMessage({ type: "spotifycookies" })
   const cookies = message.response
   const spotifyCreds = cookies
     //filter what cookies we need and map to a simple lookupobject
     .filter((cookie) => cookie.name === "sp_key" || cookie.name === "sp_dc")
-    .reduce((result, cookie) => { result[cookie.name]=cookie.value; return result; },{})
+    .reduce((result, cookie) => { result[cookie.name] = cookie.value; return result; }, {})
   //show credentials in the HTML code of the OpenPodcast website
   showCredentialsOnWebsite(spotifyCreds)
 }
 
-//init
-retrieveSpotifyCookies()
+// check if current website is the OpenPodcast website where we want to show the credentials
+function isOnOpenPodcastWebsite() {
+  return window.location.href.match(/https:\/\/openpodcast.dev\/.*connect/) && document.getElementById("openpodcast-plugin")
+}
+
+//wait 1sec until the website is loaded (maybe a problem with docsify) and then check if we are on the right website
+setTimeout(() => {
+  if (isOnOpenPodcastWebsite()) {
+    retrieveSpotifyCookies()
+  }
+}, 1000)
