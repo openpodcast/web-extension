05370d1d76d7eca1fffadbb9cf7f4bd9d20cb5b2
Wolfgang Gassler <wolfgang@gassler.org>
2023-04-11 23:49:31 +0200

    fix show anchor cookie #2

--- src/openpodcast.js
+++ src/openpodcast.js
@@ -6,6 +6,9 @@ function showCredentialsOnWebsite(creds) {
   if (creds.sp_key && creds.sp_dc) {
     message = `Spotify Data: key: ${creds['sp_key']}, dc: ${creds['sp_dc']} END`
   }
+  if (creds.anchorpw_s) {
+    message = `Anchor Data: anchorpw_s: ${creds['anchorpw_s']} END`
+  }
   document.getElementById("openpodcast-plugin").innerHTML = message
 }
 
@@ -19,7 +22,7 @@ async function retrieveSpotifyCookies() {
   const cookies = message.response
   const spotifyCreds = cookies
     //filter what cookies we need and map to a simple lookupobject
-    .filter((cookie) => cookie.name === "sp_key" || cookie.name === "sp_dc")
+    .filter((cookie) => cookie.name === "sp_key" || cookie.name === "sp_dc" || cookie.name === "anchorpw_s")
     .reduce((result, cookie) => { result[cookie.name] = cookie.value; return result; }, {})
   //show credentials in the HTML code of the OpenPodcast website
   showCredentialsOnWebsite(spotifyCreds)
