dff5de9d50718c57120e45b0f082d30db4cbb8a4
Wolfgang Gassler <wolfgang@gassler.org>
2023-04-20 09:59:32 +0200

    improved UI and button to send email, version 1.2

--- src/openpodcast.js
+++ src/openpodcast.js
@@ -1,15 +1,56 @@
 const runtime = window.browser || chrome
 
+const email = "echo@openpodcast.dev"
+let lastMessage = ""
+
+function createEmailSendButton(email, subject, body) {
+  const button = document.createElement("button")
+  button.innerHTML = "Click here to send credentials to OpenPodcast Team (via Email)"
+  button.onclick = () => {
+    window.location.href = `mailto:${email}?subject=${subject}&body=${encodeURIComponent(body)}`
+  }
+  return button
+}
+
 //insert html at the beginning of the website
 function showCredentialsOnWebsite(creds) {
-  let message = "No Spotify credentials found, are you logged in on podcasters.spotify.com? (Maybe reload this page.)"
+  let message = `No Spotify credentials found, are you logged in on <a href="https://podcasters.spotify.com" target="_blank">podcasters.spotify.com</a>? (Maybe reload this page.)`
   if (creds.sp_key && creds.sp_dc) {
     message = `Spotify Data: key: ${creds['sp_key']}, dc: ${creds['sp_dc']} END`
   }
   if (creds.anchorpw_s) {
     message = `Anchor Data: anchorpw_s: ${creds['anchorpw_s']} END`
   }
-  document.getElementById("openpodcast-plugin").innerHTML = message
+
+  //avoid refreshing the message in html
+  if (lastMessage === message) {
+    return
+  } else {
+    lastMessage = message
+  }
+
+  // create div
+  const div = document.createElement("div")
+
+  // if any creds set, show email button to send credentials to OpenPodcast team
+  if (creds.sp_key || creds.sp_dc || creds.anchorpw_s) {
+    const button = createEmailSendButton(email, "OpenPodcast Spotify Credentials", message)
+    button.style.marginBottom = "2em"
+    button.style.display = "block"
+    div.appendChild(button)
+  }
+
+  // show message
+  const p = document.createElement("p")
+  p.innerHTML = message
+  div.appendChild(p)
+
+  const parentElement = document.getElementById("openpodcast-plugin")
+  // remove all children and set new content
+  while (parentElement.firstChild) {
+    parentElement.removeChild(parentElement.firstChild)
+  }
+  parentElement.appendChild(div)
 }
 
 //communicate with background script to fetch cookies (only background script is allowed to do this)
