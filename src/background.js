179712d62ed6d908ccdb21e88ca9be88a0d2ad1e
Matthias <matthias@endler.dev>
2024-02-26 13:13:44 +0100

    Update based on best-practices / make more idiomatic

--- src/background.js
+++ src/background.js
@@ -1,93 +1,79 @@
 const RUNTIME = chrome || window.browser;
 const SPOTIFY_DOMAIN = "https://podcasters.spotify.com";
 
-// register handler for external communication (new flow)
-RUNTIME.runtime.onMessageExternal.addListener(function (
-  request,
-  sender,
-  sendResponse
-) {
-  if (request) {
-    if (request.message) {
-      if (request.message == "version") {
-        // Retrieve the extension version
-        const manifest = chrome.runtime.getManifest();
-        sendResponse({ version: manifest.version });
-      } else if (request.message == "credentials") {
-        retrieveSpotifyCookies()
-          .then((credentials) => {
-            sendResponse({ credentials: credentials });
-          })
-          .catch((error) => {
-            console.error("Error retrieving Spotify credentials:", error);
-            sendResponse({ error: "Failed to retrieve credentials" });
-          });
-        return true; // keep the message channel open for the asynchronous response
+// Register handler for external communication (new flow)
+RUNTIME.runtime.onMessageExternal.addListener(
+  async (request, sender, sendResponse) => {
+    try {
+      if (request?.message) {
+        switch (request.message) {
+          case "version":
+            const manifest = RUNTIME.runtime.getManifest();
+            sendResponse({ version: manifest.version });
+            break;
+          case "credentials":
+            const credentials = await retrieveSpotifyCookies();
+            sendResponse({ credentials });
+            return true; // Keep the message channel open for the asynchronous response
+        }
       }
+    } catch (error) {
+      console.error("Error processing request:", error);
+      sendResponse({ error: "Failed to process request" });
     }
   }
-});
+);
 
-// register handler for communication with content script (old flow)
-RUNTIME.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type && request.type === "spotifycookies") {
-    retrieveSpotifyCookies()
-      .then((spotifyCredentials) => {
-        sendResponse({ response: spotifyCredentials });
-        if (spotifyCredentials !== null) {
-          removeAllCookies(SPOTIFY_DOMAIN);
-        }
-      })
-      .catch((error) => {
-        console.error("Error retrieving Spotify credentials:", error);
-        sendResponse({ error: "Failed to retrieve credentials" });
-      });
-    return true; // Indicates that we will respond asynchronously
+// Register handler for communication with content script (old flow)
+RUNTIME.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+  try {
+    if (request.type === "spotifycookies") {
+      const spotifyCredentials = await retrieveSpotifyCookies();
+      sendResponse({ response: spotifyCredentials });
+      if (spotifyCredentials) {
+        await removeAllCookies(SPOTIFY_DOMAIN);
+      }
+      return true; // Indicates that we will respond asynchronously
+    }
+  } catch (error) {
+    sendResponse({ error: "Failed to retrieve credentials" });
   }
 });
 
-// return all cookies for a specified domain (which is allowed in manifest.json)
+// Return all cookies for a specified domain
 const getDomainCookies = async (domain) => {
-  return await RUNTIME.cookies.getAll({ url: domain });
+  return RUNTIME.cookies.getAll({ url: domain });
 };
 
-// remove a specific cookie
+// Remove a specific cookie
 const removeCookie = async (cookie) => {
   await RUNTIME.cookies.remove(cookie);
 };
 
-// remove all cookies for a specified domain
+// Remove all cookies for a specified domain
 const removeAllCookies = async (domain) => {
   const cookies = await getDomainCookies(domain);
-  cookies.forEach((cookie) => {
-    removeCookie({
+  for (let cookie of cookies) {
+    await removeCookie({
       url: domain,
       name: cookie.name,
     });
-  });
+  }
 };
 
-// retrieve Spotify cookies
+// Retrieve Spotify cookies
 const retrieveSpotifyCookies = async () => {
-  return getDomainCookies(SPOTIFY_DOMAIN).then((cookies) => {
-    const spotifyCredentials = cookies
-      .filter((cookie) =>
-        ["sp_key", "sp_dc", "anchorpw_s"].includes(cookie.name)
-      )
-      .reduce((result, cookie) => {
-        result[cookie.name] = cookie.value;
-        return result;
-      }, {});
+  const cookies = await getDomainCookies(SPOTIFY_DOMAIN);
+  const spotifyCredentials = cookies
+    .filter((cookie) => ["sp_key", "sp_dc", "anchorpw_s"].includes(cookie.name))
+    .reduce((result, cookie) => {
+      result[cookie.name] = cookie.value;
+      return result;
+    }, {});
 
-    if (
-      spotifyCredentials.sp_key ||
-      spotifyCredentials.sp_dc ||
-      spotifyCredentials.anchorpw_s
-    ) {
-      return spotifyCredentials;
-    } else {
-      // No valid credentials found yet (which is not an error)
-      return null;
-    }
-  });
+  return spotifyCredentials.sp_key ||
+    spotifyCredentials.sp_dc ||
+    spotifyCredentials.anchorpw_s
+    ? spotifyCredentials
+    : null;
 };
--- src/openpodcast.js
+++ src/openpodcast.js
@@ -1,19 +1,15 @@
 const RUNTIME = window.browser || chrome;
-
 const email = "echo@openpodcast.dev";
-let lastMessage = "";
-
-// Store the ID of the interval, which we can later use to clear
-let checkLoopTimer;
+let checkInProgress = false;
 
 function createEmailSendButton(email, subject, body) {
   const button = document.createElement("button");
   button.innerHTML =
     "Click here to send credentials to OpenPodcast Team (via Email)";
   button.onclick = () => {
-    window.location.href = `mailto:${email}?subject=${subject}&body=${encodeURIComponent(
-      body
-    )}`;
+    window.location.href = `mailto:${email}?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(body)}`;
   };
   return button;
 }
@@ -26,16 +22,13 @@ function createEmailSendButton(email, subject, body) {
 function showCredentialsOnWebsite(credentials) {
   let message = `No Spotify credentials found, are you logged in on <a href="https://podcasters.spotify.com" target="_blank">podcasters.spotify.com</a>? (Maybe reload this page.)`;
   if (credentials.sp_key && credentials.sp_dc) {
-    message = `Spotify Data: key: ${credentials["sp_key"]}, dc: ${credentials["sp_dc"]} END`;
+    message = `Spotify Data: key: ${credentials.sp_key}, dc: ${credentials.sp_dc} END`;
   }
   if (credentials.anchorpw_s) {
-    message = `Anchor Data: anchorpw_s: ${credentials["anchorpw_s"]} END`;
+    message = `Anchor Data: anchorpw_s: ${credentials.anchorpw_s} END`;
   }
 
-  // create div
   const div = document.createElement("div");
-
-  // if any credentials set, show email button to send credentials to OpenPodcast team
   if (credentials.sp_key || credentials.sp_dc || credentials.anchorpw_s) {
     const button = createEmailSendButton(
       email,
@@ -43,61 +36,62 @@ function showCredentialsOnWebsite(credentials) {
       message
     );
     button.style.marginBottom = "2em";
-    button.style.display = "block";
     div.appendChild(button);
   }
 
-  // show message
   const p = document.createElement("p");
   p.innerHTML = message;
   div.appendChild(p);
 
   const parentElement = document.getElementById("openpodcast-plugin");
-
-  if (!parentElement) {
-    // Do nothing if the parent element is not found
-    return;
+  if (parentElement) {
+    while (parentElement.firstChild) {
+      parentElement.removeChild(parentElement.firstChild);
+    }
+    parentElement.appendChild(div);
   }
-
-  // remove all children and set new content
-  while (parentElement.firstChild) {
-    parentElement.removeChild(parentElement.firstChild);
-  }
-  parentElement.appendChild(div);
 }
 
 // Communicate with background script to fetch cookies (only background script
 // is allowed to do this)
+// Credentials are already validated in the background.js
+// This returns `null` if no credentials are found
 async function retrieveSpotifyCookies() {
-  // sends a message to the background.js and retrieve cookies
-  const message = await RUNTIME.runtime.sendMessage({ type: "spotifycookies" });
-  // Credentials are returned in the response (and already validated)
-  // This returns `null` if no credentials are found
-  return message.response;
+  try {
+    const message = await RUNTIME.runtime.sendMessage({
+      type: "spotifycookies",
+    });
+    return message.response;
+  } catch (error) {
+    // No valid credentials found yet (which is not an error)
+    return null;
+  }
 }
 
 // Check if current website is the Open Podcast website where we want to show the
 // credentials. This is only used on openpodcast.dev
 function isOnOpenPodcastDevWebsite() {
-  const res =
+  return (
     /^(https?:\/\/)?(www\.)?(openpodcast\.dev|localhost)/.test(
       window.location.href
-    ) && document.getElementById("openpodcast-plugin") !== null;
-  return res;
+    ) && document.getElementById("openpodcast-plugin") !== null
+  );
 }
 
 // Function to initiate the loop
 function checkForCredentials() {
-  checkLoopTimer = setInterval(async () => {
-    if (isOnOpenPodcastDevWebsite()) {
-      const spotifyCredentials = await retrieveSpotifyCookies();
-
-      if (spotifyCredentials) {
-        showCredentialsOnWebsite(spotifyCredentials);
-
-        // Clear the timer interval since the credentials have been retrieved
-        // This will stop the loop
-        clearInterval(checkLoopTimer);
+  setInterval(async () => {
+    if (isOnOpenPodcastDevWebsite() && !checkInProgress) {
+      checkInProgress = true;
+      try {
+        const spotifyCredentials = await retrieveSpotifyCookies();
+        if (spotifyCredentials) {
+          showCredentialsOnWebsite(spotifyCredentials);
+        }
+      } catch (error) {
+        console.error("Error checking for credentials:", error);
+      } finally {
+        checkInProgress = false;
       }
     }
   }, 1000);
